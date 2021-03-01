c0b4d977d77599c3c08875bea73c0f2ca1ce54c9
guramritpalsaggu <guramritpalsaggu@gmail.com>
2021-03-01 22:51:42 +0530

    deploy

--- Deployment/sketch.js
+++ Deployment/sketch.js
@@ -1,17 +1,35 @@
-let video; 
-let poseNet; 
-let pose;  
-let skeleton;   
-let brain; 
-let poseLabel = "T";
+let w, t, video;
+let imgArr = []
+let task;
+
+let poseNet;
+let pose;
+let skeleton;
+
+let brain;
+let poseLabel = "U";
+let result;
+
+function preload(){
+  w = loadImage("w.jpeg");
+  t = loadImage("t.png");
+  u = loadImage("u.jpg")
+  task = t;
+  result = t;
+  imgArr.push(w);
+  imgArr.push(t);
+  imgArr.push(u);
+}
 
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(800, 800);
   video = createCapture(VIDEO);
   video.hide();
+  setInterval(function (){
+  task = imgArr[Math.floor(Math.random() * imgArr.length)];
+  }, 2000);
   poseNet = ml5.poseNet(video, modelLoaded);
   poseNet.on('pose', gotPoses);
-
   let options = {
     inputs: 34,
     outputs: 4,
@@ -25,7 +43,8 @@ function setup() {
     weights: 'model.weights.bin',
   };
   brain.load(modelInfo, brainLoaded);
-} 
+}
+
 function brainLoaded() {
   console.log('pose classification ready!');
   classifyPose();
@@ -45,51 +64,45 @@ function classifyPose() {
     setTimeout(classifyPose, 100);
   }
 }
-function gotResult(error, results) {
+
+function gotResult(error, results) { 
   
   if (results[0].confidence > 0.75) {
     poseLabel = results[0].label.toUpperCase();
   }
-  //console.log(results[0].confidence);
+  //console.log(results[0].confidence); 
   classifyPose();
 }
+
 function gotPoses(poses) {
   if (poses.length > 0) {
     pose = poses[0].pose;
     skeleton = poses[0].skeleton;
   }
 }
+
 function modelLoaded() {
   console.log('poseNet ready');
 }
-function draw() {
-  push();
-  translate(video.width, 0);
-  scale(-1, 1);
-  image(video, 0, 0, video.width, video.height);
 
-  if (pose) {
-    for (let i = 0; i < skeleton.length; i++) {
-      let a = skeleton[i][0];
-      let b = skeleton[i][1];
-      strokeWeight(2);
-      stroke(0);
 
-      line(a.position.x, a.position.y, b.position.x, b.position.y);
-    }
-    for (let i = 0; i < pose.keypoints.length; i++) {
-      let x = pose.keypoints[i].position.x;
-      let y = pose.keypoints[i].position.y;
-      fill(0);
-      stroke(255);
-      ellipse(x, y, 16, 16);
-    }
-  }
-  pop();
-
-  fill(255, 0, 255);
-  noStroke();
-  textSize(512);
-  textAlign(CENTER, CENTER);
-  text(poseLabel, width / 2, height / 2);
-}
+function draw() { 
+  background(220); 
+  image(video, 0, 0, 800, 400) 
+  textSize(32); 
+  text('Make this Character:', 30, 440); 
+  text('Result of the Model:', 430, 440); 
+  image(task, 0, 450, 400, 400); 
+  if (poseLabel == 'U') { 
+    result = u; 
+  } 
+  else if(poseLabel == 'T') { 
+    result = t; 
+  } 
+  else { 
+    result = w; 
+  } 
+  console.log(result); 
+  console.log(poseLabel); 
+  image(result, 400, 450, 400, 400); 
+} 
